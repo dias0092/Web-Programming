9356bd3f18e9293a7a9e3fe5d5b1e9ecdfb8d1a0
dias0092 <54438346+dias0092@users.noreply.github.com>
2020-12-03 16:07:39 +0600

    Add files via upload

---
+++ Homeworks/homework_8/script.js
@@ -0,0 +1,67 @@
+
+const quiz = document.getElementById("quiz");
+const answerUser = document.querySelectorAll(".answer");
+const quest = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    quest.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerUser.forEach((answerUser) => {
+        if (answerUser.checked) {
+            answer = answerUser.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerUser.forEach((answerUser) => {
+        answerUser.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
+
